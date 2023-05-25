7ce024578b2a776f63473665d7847966d4a4a0e0
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2023-05-25 11:30:21 +0530

    import java.util.*; public class multi {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int b=m*n;         System.out.println(b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class multi
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int b=m*n;
+        System.out.println(b);
+    }
+}
