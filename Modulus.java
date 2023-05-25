ffbf59f31b17727eb43d59a6a43f0b1e4fcb5b21
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2023-05-25 11:35:00 +0530

    import java.util.*; public class supri {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int r=n%m;         System.out.println(r);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class supri
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int r=n%m;
+        System.out.println(r);
+    }
+}
