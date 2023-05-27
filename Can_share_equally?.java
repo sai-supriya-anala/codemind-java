11af39085f5b5565d138367a4bcae69960972d16
sai-supriya-anala <120079349+sai-supriya-anala@users.noreply.github.com>
2023-05-27 09:39:52 +0530

    import java.util.Scanner; public class suppuuuu {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if(x!=0&&x%2==0||x==0&&y%2==0)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class suppuuuu
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x!=0&&x%2==0||x==0&&y%2==0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
